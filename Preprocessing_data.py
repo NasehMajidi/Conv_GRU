438c41ebbca839c100d5902864efbdf37a54cfa5
NasehMajidi <nasehmajidi1997@gmail.com>
2023-03-28 17:39:15 +0330

    Update Preprocessing_data.py

--- Preprocessing_data.py
+++ Preprocessing_data.py
@@ -15,7 +15,42 @@ from utils.Preprocessing_functions import *
 ##################################### Main ####################################
 ###############################################################################
 
-# load the preprocessed data
+# load btc data
+btc = pd.read_csv(
+  r"Dataset/bitcoin_1d.csv",  
+  parse_dates=['Date'],
+  index_col="Date"
+)
+
+dataset = pd.DataFrame()
+dataset["btc"] = btc["Adj Close"].copy()
+
+dataset.describe()
+
+# train-validation-test split
+train_size = int(len(dataset) * 0.80)
+val_size =  int(len(dataset) * 0.10)
+test_size = len(dataset) - train_size - val_size
+data = dict()
+data['train'] , data['val'] , data['test'] = dataset.iloc[0:train_size].copy(), dataset.iloc[train_size:train_size + val_size].copy() , dataset.iloc[train_size+val_size:len(dataset)].copy()
+print('------- Dataset Summary ----------')
+print("# dataset   :   ",len(dataset))
+print("# train     :   ",len(data['train']))
+print("# validation:   ",len(data['val']))
+print("# test      :   ",len(data['test']))
+print('-----------------------------------')
+
+# calculate the percentage error of price data
+for key in data.keys():
+    data[key]['pct'] = np.log(data[key]['btc'].pct_change() + 1)
+    data[key] = data[key].dropna()
+
+# scale the price data (normalization phase)
+target_column = 'pct'
+out_col = target_column + "_mm0" 
+data['train'][out_col] ,data['val'][out_col], data['test'][out_col], out_scaler = scale(data , target_column , MinMaxScaler())
+
+# save the preprocessed data
 with open('Dataset/preprocessed_data.pickle', 'wb') as handle:
     pickle.dump(data, handle)
 
@@ -63,5 +98,6 @@ for key in data_loader.keys():
     print(f"# {key}: {len(data_loader[key])}    ")
 print('-------------------------------')
 
+# saved data loder
 with open('Dataset/preprocessed_dataloader.pickle', 'wb') as handle:
     pickle.dump(data_loader, handle)
